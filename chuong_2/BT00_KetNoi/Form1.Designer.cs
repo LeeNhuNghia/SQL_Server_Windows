a612ac4f712040190e12310aa4b07a26872abddb
nghia <nghia@gmail.com>
2023-02-07 14:44:59 +0700

    Thay doi 7/2/20023

---
+++ chuong_2/BT00_KetNoi/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace BT00_KetNoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAcc2003 = new System.Windows.Forms.Button();
+            this.btnAcc2019 = new System.Windows.Forms.Button();
+            this.btnSQLWin = new System.Windows.Forms.Button();
+            this.btnSQLsa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAcc2003
+            // 
+            this.btnAcc2003.Location = new System.Drawing.Point(39, 28);
+            this.btnAcc2003.Name = "btnAcc2003";
+            this.btnAcc2003.Size = new System.Drawing.Size(88, 37);
+            this.btnAcc2003.TabIndex = 0;
+            this.btnAcc2003.Text = "Access2003";
+            this.btnAcc2003.UseVisualStyleBackColor = true;
+            this.btnAcc2003.Click += new System.EventHandler(this.btnAcc2003_Click);
+            // 
+            // btnAcc2019
+            // 
+            this.btnAcc2019.Location = new System.Drawing.Point(133, 28);
+            this.btnAcc2019.Name = "btnAcc2019";
+            this.btnAcc2019.Size = new System.Drawing.Size(88, 37);
+            this.btnAcc2019.TabIndex = 0;
+            this.btnAcc2019.Text = "Access2019";
+            this.btnAcc2019.UseVisualStyleBackColor = true;
+            this.btnAcc2019.Click += new System.EventHandler(this.btnAcc2019_Click);
+            // 
+            // btnSQLWin
+            // 
+            this.btnSQLWin.Location = new System.Drawing.Point(227, 28);
+            this.btnSQLWin.Name = "btnSQLWin";
+            this.btnSQLWin.Size = new System.Drawing.Size(88, 37);
+            this.btnSQLWin.TabIndex = 0;
+            this.btnSQLWin.Text = "SQL Windown";
+            this.btnSQLWin.UseVisualStyleBackColor = true;
+            this.btnSQLWin.Click += new System.EventHandler(this.btnSQLWin_Click);
+            // 
+            // btnSQLsa
+            // 
+            this.btnSQLsa.Location = new System.Drawing.Point(321, 28);
+            this.btnSQLsa.Name = "btnSQLsa";
+            this.btnSQLsa.Size = new System.Drawing.Size(88, 37);
+            this.btnSQLsa.TabIndex = 0;
+            this.btnSQLsa.Text = "SQL sa";
+            this.btnSQLsa.UseVisualStyleBackColor = true;
+            this.btnSQLsa.Click += new System.EventHandler(this.btnSQLsa_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(446, 94);
+            this.Controls.Add(this.btnSQLsa);
+            this.Controls.Add(this.btnSQLWin);
+            this.Controls.Add(this.btnAcc2019);
+            this.Controls.Add(this.btnAcc2003);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kết nối";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAcc2003;
+        private System.Windows.Forms.Button btnAcc2019;
+        private System.Windows.Forms.Button btnSQLWin;
+        private System.Windows.Forms.Button btnSQLsa;
+    }
+}
+
---
+++ chuong_2/BT00_KetNoi/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb; //sử dụng khi kết nối và làm việc với Access
+using System.Data.SqlClient; // sử dụng khi kết nối và làm việc với SQL Server
+
+namespace BT00_KetNoi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcc2003_Click(object sender, EventArgs e)
+        {
+            //Khai báo các đối tượng cần sử dụng để tạo kết nối
+            //1. Chuỗi kết nối đến Access 2003
+            string strcon = @"provider=microsoft.jet.oledb.4.0; data source = ..\..\..\Data\QL_SINHVIEN.mdb";
+            //2. Khai báo đối tượng kết nối
+            OleDbConnection cnn = new OleDbConnection(strcon);
+            //3. Mở kết nối
+            cnn.Open();
+            //4. Kiểm tra kết nối có được mở không
+            if(cnn.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Kết nối đã mở.");
+            }
+            //5. Đóng kết nối
+            cnn.Close();
+        }
+
+        private void btnAcc2019_Click(object sender, EventArgs e)
+        {
+            //Khai báo các đối tượng cần sử dụng để tạo kết nối
+            //1. Chuỗi kết nối đến Access 2019
+            string strcon = @"provider=microsoft.ACE.oledb.12.0; data source = ..\..\..\Data\QL_SINHVIEN.mdb";
+            //2. Khai báo đối tượng kết nối
+            OleDbConnection cnn = new OleDbConnection(strcon);
+            //3. Mở kết nối
+            cnn.Open();
+            //4. Kiểm tra kết nối có được mở không
+            if (cnn.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Kết nối đã mở.");
+            }
+            //5. Đóng kết nối
+            cnn.Close();
+        }
+
+        private void btnSQLWin_Click(object sender, EventArgs e)
+        {
+            //Khai báo các đối tượng cần sử dụng để tạo kết nối
+            //1. Chuỗi kết nối đến SQL Server bằng user của Windows
+            string strcon = @"server=.; database=C21TH2_LTCSDL; integrated security=true";
+            //2. Khai báo đối tượng kết nối
+            SqlConnection cnn = new SqlConnection(strcon);
+            //3. Mở kết nối
+            cnn.Open();
+            //4. Kiểm tra kết nối có được mở không
+            if (cnn.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Kết nối đã mở.");
+            }
+            //5. Đóng kết nối
+            cnn.Close();
+        }
+
+        private void btnSQLsa_Click(object sender, EventArgs e)
+        {
+            //Khai báo các đối tượng cần sử dụng để tạo kết nối
+            //1. Chuỗi kết nối đến SQL Server bằng user của Windows
+            string strcon = @"server=.; database=C21TH2_LTCSDL; uid=sa; pwd=c21th";
+            //2. Khai báo đối tượng kết nối
+            SqlConnection cnn = new SqlConnection(strcon);
+            //3. Mở kết nối
+            cnn.Open();
+            //4. Kiểm tra kết nối có được mở không
+            if (cnn.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Kết nối đã mở.");
+            }
+            //5. Đóng kết nối
+            cnn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
